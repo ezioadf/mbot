5d4a339a63a55b9e1d0152a777ca86ce64408d9d
rorschach <manideep@testbook.com>
2017-03-29 12:42:42 +0530

    getting port from environment

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"io"
 	"fmt"
+	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +15,10 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", hello)
 	fmt.Println("Starting server on port 3001")
-	http.ListenAndServe(":3001", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3001"
+	}
+	http.ListenAndServe(":"+port, nil)
 	return
 }
